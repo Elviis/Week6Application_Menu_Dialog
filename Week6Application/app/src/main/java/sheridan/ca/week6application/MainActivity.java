dbc8633e350836b93a651a620b5b902e266a4637
Elviis <jonathansakowski@Gmail.com>
2016-04-20 12:11:36 -0400

    Initial commit

---
+++ Week6Application/app/src/main/java/sheridan/ca/week6application/MainActivity.java
@@ -0,0 +1,91 @@
+package sheridan.ca.week6application;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+     TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+         textView = (TextView) findViewById(R.id.text_view);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    public void resetText(View view){
+
+    //
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(" Would you Like to Reset Text ?");
+         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(DialogInterface dialog, int which) {
+                 //
+                 textView.setText("Hello Android");
+             }
+         });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+             // action
+            return true;
+        }
+        if(id== R.id.action_message){
+            // action
+
+            Toast.makeText(this, " message item is clicked",Toast.LENGTH_LONG).show();
+            return  true;
+        }
+        if(id== R.id.action_share){
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
